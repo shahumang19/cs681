5870900affeceaafb7d409333c56e85a06ed90cd
umangshah <shahumang19@gmail.com>
2023-04-30 22:56:58 -0400

    HW11 Completed

--- src/edu/umb/cs681/hw11/RequestHandler.java
+++ src/edu/umb/cs681/hw11/RequestHandler.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RequestHandler implements Runnable  {
-    private final AtomicBoolean stop = new AtomicBoolean(false);
+    private AtomicBoolean stop = new AtomicBoolean(false);
     private Random random = new Random(0);
     private Path[] files = {
         Path.of("data/hw11/a.html"), Path.of("data/hw11/b.html"), Path.of("data/hw11/c.html") 
@@ -32,6 +32,36 @@ public class RequestHandler implements Runnable  {
 
     public void stop(){
         stop.set(true);
+    }
+
+    public static void main(String[] args) {
+        int threadCount = 20;
+        Thread[] threads = new Thread[threadCount];
+        RequestHandler[] handlers = new RequestHandler[threadCount];
+
+        for (int i = 0; i < threadCount; i++) {
+            handlers[i] = new RequestHandler();
+            threads[i] = new Thread(handlers[i]);
+            threads[i].start();
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < threadCount; i++) {
+            handlers[i].stop();
+            threads[i].interrupt();
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
     
 }
